69787f6dd6a50a7576f3b13748569d84fa7ec080
Paul Martin Panez Caballero <ppanezuni@gmail.com>
2022-11-22 19:26:59 -0500

    subiendo a bigquery data de senamhi limpia

--- procesamiento/raw.py
+++ procesamiento/raw.py
@@ -10,10 +10,10 @@ def main():
             big_query.cargar_datos("gs://dataminers-369202/minsa.csv", tabla)
         tabla = big_query.crear_tabla(dataset, "senamhiCO")
         if tabla:
-            big_query.cargar_datos("gs://dataminers-369202/senamhiCO.csv", tabla)
+            big_query.cargar_datos("gs://dataminers-369202/SenamhiCO_limpio_2.txt", tabla)
         tabla = big_query.crear_tabla(dataset, "senamhiEMA")
         if tabla:
-            big_query.cargar_datos("gs://dataminers-369202/senamhiEMA.csv", tabla)
+            big_query.cargar_datos("gs://dataminers-369202/SenamhiEMA_limpio_2.txt", tabla)
 
 
 if __name__ == "__main__":

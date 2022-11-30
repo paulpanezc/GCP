34b7c2e0a56c30bc027741c811ad301f0b2d3b5e
Paul Martin Panez Caballero <ppanezuni@gmail.com>
2022-11-29 23:02:16 -0500

    corrigiendo vulneracion de API_KEY

--- deploy/web_app/home/views.py
+++ deploy/web_app/home/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from ubigeo.models import Departamento
@@ -9,4 +10,5 @@ class HomePageView(TemplateView):
         context = super(HomePageView, self).get_context_data(**kwargs)
         departamentos = Departamento.objects.all().order_by('nombre')
         context['departamentos'] = departamentos
+        context['API_KEY'] = settings.GOOGLE_MAPS_API_KEY
         return context

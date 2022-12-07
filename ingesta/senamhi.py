1aa564825a935cd0c5907bf1699ce30af91e3979
Paul Martin Panez Caballero <ppanezuni@gmail.com>
2022-12-07 13:00:31 -0500

    version 0.8 se agrego excepcion encontrada al ejecutar trabajo PySpark en Dataproc

--- ingesta/senamhi.py
+++ ingesta/senamhi.py
@@ -24,52 +24,56 @@ def parseo(tipo_estacion, codigo_estacion, periodo, altitud):
           "cod_old=&cate_esta={}&soloAlt={}".format(periodo, codigo_estacion, estaciones[tipo_estacion]["estado"],
                                                     tipo_estacion, altitud)
     print(url)
-    r = requests.get(url)
-    web = r.text
-    scraping = BeautifulSoup(web, "html.parser")
-    tabla_datos = scraping("table")[0]
-    tabla_mediciones = scraping("table")[1]
-    estacion = tabla_datos("tr")[0].text.strip().split(" : ")[1]
-    departamento = tabla_datos("tr")[1]("td")[1].text
-    provincia = tabla_datos("tr")[1]("td")[3].text
-    distrito = tabla_datos("tr")[1]("td")[5].text
-    latitud = tabla_datos("tr")[2]("td")[1].text
-    longitud = tabla_datos("tr")[2]("td")[3].text
-    altitud = tabla_datos("tr")[2]("td")[5].text
-    tipo = tabla_datos("tr")[3]("td")[1].text
-    codigo = tabla_datos("tr")[3]("td")[3].text
-    i = 0
     contenido = ""
-    for fila in tabla_mediciones("tr"):
-        if tipo_estacion == "CO":
-            if i > 1:
-                fecha = fila("td")[0].text.strip()
-                temperatura_max = fila("td")[1].text.strip()
-                temperatura_min = fila("td")[2].text.strip()
-                humedad = fila("td")[3].text.strip()
-                precipitacion = fila("td")[4].text.strip()
-                contenido += "{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(estacion, departamento, provincia,
-                                                                                  distrito, latitud, longitud, altitud,
-                                                                                  tipo, codigo, fecha, temperatura_max,
-                                                                                  temperatura_min, humedad,
-                                                                                  precipitacion)
-        elif tipo_estacion == "EMA":
-            if i > 0:
-                fecha = fila("td")[0].text.strip()
-                hora = fila("td")[1].text.strip()
-                temperatura = fila("td")[2].text.strip()
-                precipitacion = fila("td")[3].text.strip()
-                humedad = fila("td")[4].text.strip()
-                direccion_viento = fila("td")[5].text.strip()
-                velocidad_viento = fila("td")[6].text.strip()
-                contenido += "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(estacion, departamento,
-                                                                                        provincia, distrito, latitud,
-                                                                                        longitud, altitud, tipo, codigo,
-                                                                                        fecha, hora, temperatura,
-                                                                                        precipitacion, humedad,
-                                                                                        direccion_viento,
-                                                                                        velocidad_viento)
-        i += 1
+    try:
+        r = requests.get(url)
+        web = r.text
+        scraping = BeautifulSoup(web, "html.parser")
+        tabla_datos = scraping("table")[0]
+        tabla_mediciones = scraping("table")[1]
+        estacion = tabla_datos("tr")[0].text.strip().split(" : ")[1]
+        departamento = tabla_datos("tr")[1]("td")[1].text
+        provincia = tabla_datos("tr")[1]("td")[3].text
+        distrito = tabla_datos("tr")[1]("td")[5].text
+        latitud = tabla_datos("tr")[2]("td")[1].text
+        longitud = tabla_datos("tr")[2]("td")[3].text
+        altitud = tabla_datos("tr")[2]("td")[5].text
+        tipo = tabla_datos("tr")[3]("td")[1].text
+        codigo = tabla_datos("tr")[3]("td")[3].text
+        i = 0
+        for fila in tabla_mediciones("tr"):
+            if tipo_estacion == "CO":
+                if i > 1:
+                    fecha = fila("td")[0].text.strip()
+                    temperatura_max = fila("td")[1].text.strip()
+                    temperatura_min = fila("td")[2].text.strip()
+                    humedad = fila("td")[3].text.strip()
+                    precipitacion = fila("td")[4].text.strip()
+                    contenido += "{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(estacion, departamento, provincia,
+                                                                                      distrito, latitud, longitud, altitud,
+                                                                                      tipo, codigo, fecha, temperatura_max,
+                                                                                      temperatura_min, humedad,
+                                                                                      precipitacion)
+            elif tipo_estacion == "EMA":
+                if i > 0:
+                    fecha = fila("td")[0].text.strip()
+                    hora = fila("td")[1].text.strip()
+                    temperatura = fila("td")[2].text.strip()
+                    precipitacion = fila("td")[3].text.strip()
+                    humedad = fila("td")[4].text.strip()
+                    direccion_viento = fila("td")[5].text.strip()
+                    velocidad_viento = fila("td")[6].text.strip()
+                    contenido += "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(estacion, departamento,
+                                                                                            provincia, distrito, latitud,
+                                                                                            longitud, altitud, tipo, codigo,
+                                                                                            fecha, hora, temperatura,
+                                                                                            precipitacion, humedad,
+                                                                                            direccion_viento,
+                                                                                            velocidad_viento)
+            i += 1
+    except requests.exceptions.ConnectionError:
+        print("No se pudo obtener la data climatológica de la estación {} para el periodo {}.".format(codigo_estacion,
+                                                                                                      periodo))
     return contenido
 
 
